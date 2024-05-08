b7f55ee2045e5a46c2c4d0e1f951744258384113
Arielton97 <arieltonribeiro97@gmail.com>
2024-05-07 22:56:45 -0300

    desenvolvimento do estilo com bootstrap

--- assets/util/consultar.php
+++ assets/util/consultar.php
@@ -4,80 +4,90 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Consultar Funcionário no formulário</title>
+    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../../assets/css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="../../assets/css/style.css">
 </head>
 <body>
-    <p><a href="../../index.php">Voltar</a></p>
-    <div>
-        <h1>Formulário de Funcionários</h1>
-        <p>Lista de funcionários cadastrados no formulário</p>
-        <?php
-            //  Obtém a lista de hábitos do banco de dados MySQL
-            require_once 'conexao.php';
+    <main>
+        <nav>
+            <ul class="nav nav-tabs">
+                <li class="nav-item">
+                    <a href="../../index.php" class="nav-link active">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a href="cadastro.html" class="nav-link active">Cadastrar</a>
+                </li>
+            </ul>
+        </nav>
+        <div>
+            <h1>Formulário de Funcionários</h1>
+            <p>Lista de funcionários cadastrados no formulário</p>
+            <?php
+                //  Obtém a lista de hábitos do banco de dados MySQL
+                require_once 'conexao.php';
 
-            //Executa a query/ consulta da variável $sql
-            $sql = " SELECT * FROM funcionarios";
-            $resultado = $conexao->query($sql);
+                //Executa a query/ consulta da variável $sql
+                $sql = " SELECT * FROM funcionarios";
+                $resultado = $conexao->query($sql);
 
-            if(isset($_SESSSION['mensagem'])) {
-                echo "
-                <script>
-                    let escrever = `{$_SESSION['mensagem']}`;
-                    let msg = document.getElementById('aviso');
-                    msg.innerHTML = escrever;
-                    setInterval(function(){
-                        msg.innerHTML = '';
-                    }, 3000)
-                </script>";
-            }
-            session_unset();
+                session_unset();
 
-            //  Verifica se a query retornou registros
-            if ($resultado->num_rows > 0) {
-                
-                ?>
-                <br />
-                <table border="1px">
-                    <tbody>
-                        <tr>
-                            <th>Codigo</th>
-                            <th>Nome</th>
-                            <th>Cargo</th>
-                            <th>Descrição do Cargo</th>
-                            <th>Setor</th>
-                            <th>Salário</th>
-                        </tr>
-                        <?php
-                        //  Looping pelos registros retornados
-                        while($registro = $resultado->fetch_assoc()) {
-                            ?>
-                            <tr> 
-                                <td> <?php echo $registro["codigo"]; ?> </td>
-                                <td> <?php echo $registro["nome"]; ?> </td>
-                                <td> <?php echo $registro["cargo"]; ?> </td>
-                                <td> <?php echo $registro["descricaodocargo"]; ?> </td>
-                                <td> <?php echo $registro["setor"]; ?> </td>
-                                <td> <?php echo $registro["salario"]; ?> </td>
-                                <td> <?php echo "<a href='editar.php?id=" . $registro['codigo'] . "'>" . "Editar" . "</a>" ;?></td> 
-                                <td> <?php echo "<a href='apagar.php?id=" . $registro['codigo'] . "'>" . "Apagar" . "</a>" ;?></td>
+                //  Verifica se a query retornou registros
+                if ($resultado->num_rows > 0) {
+                    
+            ?>
+                    <br />
+                    <table border="1px">
+                        <tbody>
+                            <tr>
+                                <th>Codigo</th>
+                                <th>Nome</th>
+                                <th>Cargo</th>
+                                <th>Descrição do Cargo</th>
+                                <th>Setor</th>
+                                <th>Salário</th>
                             </tr>
-                        <?php
-                        } // fim do looping
-                        ?>  
-                    </tbody>
-                </table>
+                            <?php
+                            //  Looping pelos registros retornados
+                            while($registro = $resultado->fetch_assoc()) {
+                                ?>
+                                <tr> 
+                                    <td> <?php echo $registro["codigo"]; ?> </td>
+                                    <td> <?php echo $registro["nome"]; ?> </td>
+                                    <td> <?php echo $registro["cargo"]; ?> </td>
+                                    <td> <?php echo $registro["descricaodocargo"]; ?> </td>
+                                    <td> <?php echo $registro["setor"]; ?> </td>
+                                    <td> <?php echo $registro["salario"]; ?> </td>
+                                    <td> <?php echo "<a href='editar.php?id=" . $registro['codigo'] . "'>" . "Editar" . "</a>" ;?></td> 
+                                    <td> <?php echo "<a href='apagar.php?id=" . $registro['codigo'] . "'>" . "Apagar" . "</a>" ;?></td>
+                                </tr>
+                            <?php
+                            } // fim do looping
+                            ?>  
+                        </tbody>
+                    </table>
 
-                <p>Cadastrar mais funcionários no formulário ?</p>
-                <?php
+                    
+                    <?php
             } else {
-                ?>
-                <p>Frase do dia</p>
-                <p>Quem não tem coragem de arriscar, não chega a lugar nenhum</p>
-                <?php
-            } //    fim do if
-        //  fecha a coneão com o MySQL
-        $conexao->close();
-        ?>
-        <a href="cadastro.php">Cadastrar Funcionário</a>
-    </div>
+                    ?>
+                    <p>Frase do dia</p>
+                    <p>Quem não tem coragem de arriscar, não chega a lugar nenhum</p>
+                    <?php
+                } //    fim do if
+            //  fecha a coneão com o MySQL
+            $conexao->close();
+            ?>
+
+            <!-- Botão para cadastrar funcionários -->
+            <p>Cadastrar mais funcionários no formulário ?</p>
+            <button type="button" class="btn btn-primary btn-lg">
+                <a href="cadastro.html" id="botao">Cadastrar Funcionário</a>
+            </button>
+            
+        </div>
+    </main>
+    
 </body>
 </html>
--- index.php
+++ index.php
@@ -4,21 +4,48 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css.map">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
-    <!-- Logo da UNIASSELVi -->
-    <div class="logo_uniasselvi">
-        <img src="assets/img/uniasselvi-logo.jpeg" alt="logo_uniasselvi_png">
-    </div>
+    <script src="assets/js/script.js"></script>
+    <!-- Navegação -->
+    <header>
+        <div class="container" id="nav-container">
+            <nav>
+                <!-- Logo da UNIASSELVi -->
+                <a href="#">
+                    <img src="assets/img/uniasselvi-logo.jpeg" alt="logo_uniasselvi_png">
+                </a>
+                <ul class="nav nav-tabs">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link active">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link disabled">Sair</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        
+    </header>
+        
 
-    <!-- Titulo para o CRUD - UNIASSELVI - e links para cadastro e consulta -->
-    <div>
-    <h1 class="titulo_uniasselvi">CRUD simplista para a UNIASSELVI</h1>
-    <div class="links">
-        <button><a href="assets/util/cadastro.php">Cadastro de funcionário</a></button>
-        <button><a href="assets/util/consultar.php">Consultar funcionários no formulário</a></button>
-    </div>
-    </div>
+    <main class"container">
+        
+        <!-- Titulo para o CRUD - UNIASSELVI - e links para cadastro e consulta -->
+        <div>
+        <h1 class="">CRUD simplista para a UNIASSELVI</h1>
+        <div class="">
+            <button type="button" class="btn btn-primary btn-lg">
+                <a href="assets/util/cadastro.html" id="botao">Cadastro de funcionário</a>
+            </button>
+            <button type="button" class="btn btn-primary btn-lg">
+                <a href="assets/util/consultar.php" id="botao">Consultar funcionários no formulário</a>
+            </button>
+        </div>
+        </div>
+    </main>
 </body>
 </html>
